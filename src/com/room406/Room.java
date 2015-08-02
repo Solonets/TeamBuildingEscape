cd0f6cb4ebc89a84fb6d092afaaae3bc5abcb5d9
solonets <ssolonets@gmail.com>
2015-08-02 17:37:15 +0300

    Room class

---
+++ src/com/room406/Room.java
@@ -0,0 +1,14 @@
+package com.room406;
+
+import java.util.List;
+
+/**
+ * Created by user on 02.08.2015.
+ */
+public class Room {
+
+    List<Room> getAvailiableRooms()
+    {
+        return null;
+    }
+}
